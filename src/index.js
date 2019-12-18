809fa09c8eb9f1e5954a27c2e5fb087048ff1e4e
DCook <declancook44@gmail.com>
2019-12-18 18:00:05 +0000

    Add router and basic shape

--- src/index.js
+++ src/index.js
@@ -1,19 +1,45 @@
+const Router = require('./router');
 
-async function handleRequest(request) {
+const downloadHeaders =
+{
+    'Content-Description': 'File Transfer',
+    'Content-Type': 'application/octet-stream',
+    'Content-Disposition': 'attachment; filename=empty.dat',
+    'Content-Transfer-Encoding': 'binary',
+    'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0',
+    'Cache-Control': 'post-check=0, pre-check=0',
+    'Pragma': 'no-cache',
+};
+
+const blobRegex = '/blob/(.*)';
+
+async function empty() {
     const init = {
-        headers: {
-            'Content-Description': 'File Transfer',
-            'Content-Type': 'application/octet-stream',
-            'Content-Disposition': 'attachment; filename=random.dat',
-            'Content-Transfer-Encoding': 'binary',
-            'Cache-Control': 'no-store, no-cache, must-revalidate, max-age=0',
-            'Cache-Control': 'post-check=0, pre-check=0',
-            'Pragma': 'no-cache',
-        },
+        headers: downloadHeaders
     };
 
-    var data = new Uint32Array(1048576);
+    var data = new Uint32Array(0);
     return new Response(data, init);
+}
+
+async function blob(request) {
+    const init = {
+        headers: downloadHeaders
+    };
+
+    const url = new URL(request.url);
+    const size = Number(url.pathname.match(blobRegex)[1]);
+
+    var data = new Uint32Array(Math.max(Math.min(5000000, size), 0));
+    return new Response(data, init);
+}
+
+
+async function handleRequest(request) {
+    const r = new Router();
+    r.get('/empty', empty);
+    r.get(blobRegex, blob);
+    return await r.route(request);
 }
 
 addEventListener('fetch', event => {
